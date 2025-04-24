dd1aa3fc06d1c1063f7ac4db89aee2d0d7e1cb48
Jose Miguel Pulgarin Agudelo <jjosemii64@gmail.com>
2025-04-23 22:04:16 -0500

    Add Search component  with initial form structure

---
+++ src/views/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <h1>Rick & Morty Explorer</h1>
+    <v-form v-model="search">
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            variant="outlined"
+            label="Character name"
+            required
+            ></v-text-field>
+          </v-col>
+            <v-btn variant="tonal">
+            Search
+            </v-btn>
+        </v-row>
+      </v-form>
+  </v-container>
+</template>
