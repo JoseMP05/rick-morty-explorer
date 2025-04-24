b6a178f38c2e4ecc5a6f720be4d80a2972717d16
Jose Miguel Pulgarin Agudelo <jjosemii64@gmail.com>
2025-04-23 22:53:20 -0500

    Refactor Search component to render all characters coincidence

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,39 +2,43 @@
   <v-container>
     <h1>Rick & Morty Explorer</h1>
     <v-form v-model="search">
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="nameCharacter"
-            :rules="nameRules"
-            variant="outlined"
-            label="Character name"
-            required
-            ></v-text-field>
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="nameCharacter"
+              :rules="nameRules"
+              variant="outlined"
+              label="Character name"
+              required
+              >
+            </v-text-field>
           </v-col>
-            <v-btn @click="fetchCharacter" variant="tonal">
-            Search
-            </v-btn>
+          <v-btn @click="fetchCharacter" variant="tonal">
+          Search
+          </v-btn>
         </v-row>
       </v-form>
 
       <v-alert v-if="error" type="error" class="mt-4">
           {{ error }}
         </v-alert>
-
-      <v-card v-if="character" class="mt-4" max-width="400">
-        <v-img :src="character.image" height="200px"></v-img>
-        <v-card-title>{{ character.name }}</v-card-title>
-        <v-card-subtitle>
-          {{ character.status }} - {{ character.species }}
-        </v-card-subtitle>
-        <v-card-text>
-          Origen: {{ character.origin.name }}
-        </v-card-text>
-      </v-card>
+      <v-row>
+        <v-col cols="4" v-for="character in characters" :key="character.id">
+          <v-card class="mt-4" max-width="400">
+            <v-img :src="character.image" height="200px"></v-img>
+            <v-card-title>{{ character.name }}</v-card-title>
+            <v-card-subtitle>
+              {{ character.status }} - {{ character.species }}
+            </v-card-subtitle>
+            <v-card-text>
+              Origen: {{ character.origin?.name }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
   </v-container>
 </template>
 
@@ -42,12 +46,12 @@
   import { ref } from 'vue'
 
   const nameCharacter = ref('')
-  const character = ref(null)
+  const characters = ref(null)
   const error = ref('')
   const loading = ref(false)
 
   async function fetchCharacter() {
-    character.value = null
+    characters.value = null
     error.value = ''
     loading.value = true
 
@@ -59,8 +63,8 @@
       }
       const data = await res.json()
 
-      console.log(data)
-      character.value = data.results[0]
+      characters.value = data.results
+      console.log(characters.value)
     } catch (err) {
       error.value = `Something went wrong: ${err}`
     } finally {
