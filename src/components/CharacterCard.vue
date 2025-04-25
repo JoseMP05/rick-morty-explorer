83fda79c78e38ab0156b6455cd59b4fdf50bdc94
Jose Miguel Pulgarin Agudelo <jjosemii64@gmail.com>
2025-04-25 00:59:15 -0500

    Refactor CharacterCard component to improve status display and enforce prop requirements

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -3,7 +3,8 @@
     <v-img :src="image" aspect-ratio="1/1"></v-img>
     <v-card-title>{{ name }}</v-card-title>
     <v-card-subtitle>
-      {{ status }} - {{ species }}
+    {{ species }} - 
+    <span :class="statusClass" class="py-0 px-3 rounded-pill">{{ status }}</span>
     </v-card-subtitle>
     <v-card-text>
       Origen: {{ origin?.name }}
@@ -12,15 +13,27 @@
 </template>
 
 <script setup lang="ts">
+  import { Status } from '@/api/rick&mortyApi'
+import { computed } from 'vue';
 
-  defineProps({
+  const props = defineProps({
     name: { type: String, required: true },
-    status: { type: String, required: false },
+    status: { type: String, required: true },
     species: String,
     origin: {
       type: Object as () => { name: string } | null,
       required: false
     },
-    image: { type: String, required: false }
+    image: { type: String, required: true }
+  })
+  
+  const statusClass = computed(() => {
+
+    if(props.status === Status.Alive)
+      return "bg-green-darken-2"
+    else if(props.status === Status.Dead)
+      return "bg-red-darken-2"
+
+    return "bg-grey-darken-1"
   })
 </script>
